70ba088d607dc643c41cd8ce89875a2e165a27fb
linjiafengyang <1498772665@qq.com>
2017-04-12 17:42:52 +0800

    HW7

---
+++ 15331046_陈志扬_HW7/HW7_code/HW7_code/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace HW7_code
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        // 点击手机号码归属地查询，跳转到新页面
+        private void phone_search(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(NewPage1));
+        }
+        // 点击天气预报查询，跳转到新页面
+        private void weather_search(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(NewPage2));
+        }
+    }
+}
---
+++ 15331046_陈志扬_HW7/HW7_code/HW7_code/NewPage1.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace HW7_code
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class NewPage1 : Page
+    {
+        public NewPage1()
+        {
+            this.InitializeComponent();
+        }
+        private void phoneQuery(object sender, RoutedEventArgs e)
+        {
+            location.Text = "";
+            runner.Text = "";
+            queryAsyncJSON(number.Text);
+        }
+        private void phoneReturn(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+        async void queryAsyncJSON(string tel)
+        {
+            // 先判断手机号码是否有输入
+            // 否，弹出错误信息
+            if (tel == "")
+            {
+                var i = new MessageDialog("输入手机号码不能为空！").ShowAsync();
+            }
+            else
+            {
+                string url = "http://route.showapi.com/6-1?showapi_appid=35441&showapi_sign=4d6fec3edc1248cf865020a73400088a" + "&num=" + tel;
+                HttpClient client = new HttpClient();
+                string result = await client.GetStringAsync(url);
+                JObject jobject = (JObject)JsonConvert.DeserializeObject(result);
+                // 查询成功，进行归属地、运营商的赋值
+                if (jobject["showapi_res_body"]["ret_code"].ToString() == "0")
+                {
+                    location.Text = jobject["showapi_res_body"]["prov"].ToString() + jobject["showapi_res_body"]["city"].ToString();
+                    runner.Text = "中国" + jobject["showapi_res_body"]["name"].ToString();
+                }
+                // 否，输出错误信息
+                else
+                {
+                    var i = new MessageDialog("输入的手机号码信息有误！").ShowAsync();
+                }
+            }
+        }
+    }
+}
